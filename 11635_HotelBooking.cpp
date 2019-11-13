2cc147468edbce700cd63ea1383bb0dec8d74d37
Parker Chang <parker.chang@innoservfa.com>
2019-11-13 15:24:42 +0800

    Update problem 11635

--- 11635_HotelBooking.cpp
+++ 11635_HotelBooking.cpp
@@ -19,6 +19,16 @@ struct CityNode
     int destination;        // 目的 city 號碼
     int distance;           // 與目的 city 的距離
     CityNode(int dest, int dist) { destination = dest; distance = dist; }
+
+    bool operator> (const CityNode &node) const
+    {
+        return (distance > node.distance);
+    }
+
+    bool operator< (const CityNode &node) const
+    {
+        return (distance < node.distance);
+    }
 };
 
 std::vector<CityNode> city_list[MAX_CITIES];
@@ -73,25 +83,28 @@ void shortestPath(int source)
 }
 
 // 使用 Dijkstra 演算法找出從 source 到任何一點的最短路徑
-// Note: 答案是錯的
 bool visited[MAX_CITIES];
 void dijkstra(int source)
 {
     memset(visited, 0, sizeof(visited));
     std::fill(distance_from_source, distance_from_source + city_count + 1, 1e9);
 
-    std::queue<int> calc_queue;
-    calc_queue.push(source);
+    // 注意: 這裡使用的是 priority queue
+    std::priority_queue<CityNode, std::vector<CityNode>, std::greater<CityNode> > calc_queue;
+    calc_queue.emplace(source, 0);
     distance_from_source[source] = 0;
 
     for(int n = 0; n < city_count; n++) {
-        while(!calc_queue.empty() && visited[calc_queue.front()])
+        // 注意: 由於 STL 的 priority queue 無法更新元素的內容
+        //       所以這裡的作法是將更新過後的內容重新 push 入 priority queue
+        //       在這裡要判斷 queue 頂端的 node 有沒有拜訪過了, 如果有的話就將這個 node 忽略
+        while(!calc_queue.empty() && visited[calc_queue.top().destination])
             calc_queue.pop();
 
         if(calc_queue.empty())
             break;
 
-        int current_city = calc_queue.front();
+        int current_city = calc_queue.top().destination;
         calc_queue.pop();
         visited[current_city] = true;
 
@@ -102,56 +115,10 @@ void dijkstra(int source)
                 int new_distance = distance_from_source[current_city] + next_city.distance;
                 if(new_distance < distance_from_source[next_city.destination]) {
                     distance_from_source[next_city.destination] = new_distance;
-                    calc_queue.push(next_city.destination);
+                    calc_queue.emplace(next_city.destination, new_distance);
                 }
             }
         }
-    }
-}
-
-// Note: 會超出 4 秒時間限制
-void dijkstra_2(int source)
-{
-    memset(visited, 0, sizeof(visited));
-    std::fill(distance_from_source, distance_from_source + city_count + 1, 1e9);
-
-    distance_from_source[source] = 0;
-    int candidate_city = source;
-
-    for(int n = 0, _max_n = city_count - 1; n < _max_n; n++) {
-        visited[candidate_city] = true;
-
-        for(int i = 0, _max_i = city_list[candidate_city].size(); i < _max_i; i++) {
-            CityNode next_city = city_list[candidate_city][i];
-
-            int new_distance = distance_from_source[candidate_city] + next_city.distance;
-            if(new_distance < distance_from_source[next_city.destination]) {
-                distance_from_source[next_city.destination] = new_distance;
-            }
-        }
-
-        int min_distance = 1e9;
-        int head_candidate_city = -1;
-        int new_candidate_city = -1;
-        for(int i = 1; i <= city_count; i++) {
-            if(visited[i])
-                continue;
-
-            if(head_candidate_city < 0)
-                head_candidate_city = i;
-
-            if(distance_from_source[i] < min_distance) {
-                min_distance = distance_from_source[i];
-                new_candidate_city = i;
-            }
-        }
-
-        if(new_candidate_city < 0)
-            new_candidate_city = head_candidate_city;
-        if(new_candidate_city < 0)
-            break;
-
-        candidate_city = new_candidate_city;
     }
 }
 
@@ -229,8 +196,8 @@ int main(int argc, char *argv[])
 
         for(int src_h = 0; src_h < hotel_count; src_h++) {
             hotel_booking_count[src_h][src_h] = 0;
-            shortestPath(hotel_to_city_map[src_h]);
-            //dijkstra(hotel_to_city_map[src_h]);
+            //shortestPath(hotel_to_city_map[src_h]);
+            dijkstra(hotel_to_city_map[src_h]);
             //dijkstra_2(hotel_to_city_map[src_h]);
 
             for(int dest_h = 0; dest_h < hotel_count; dest_h++) {
